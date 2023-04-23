32e40a3eae2909fc40fe20825a400debce92eeb5
fahfiily <63070156@kmitl.ac.th>
2023-04-23 16:16:29 +0700

    Add Loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Home from './screen/Home'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './screen/Home'
+import Login from './screen/Login'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
