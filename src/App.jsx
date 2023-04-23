70a4f02264925576a892e9f63d215bd6f3f20603
fahfiily <63070156@kmitl.ac.th>
2023-04-23 16:29:57 +0700

    Revert "Add Loginpage"
    This reverts commit 32e40a3eae2909fc40fe20825a400debce92eeb5.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './screen/Home'
-import Login from './screen/Login'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
